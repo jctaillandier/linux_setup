022b8893c8b47016bea9c6867e7a2e9bb47f1432
JC <jc.taillandier@hotmail.com>
2020-02-01 13:27:32 -0500

    zshrc install ok

--- zshrc_setup.sh
+++ zshrc_setup.sh
@@ -1,13 +1,13 @@
 echo "Installing Oh-My-Zsh..."
-if ["$SHELL"="zshrc"]
+
+if [ $SHELL="/usr/bin/zsh" ]
 then
 	echo "zshrc already setup as shell"
 fi
-if ["$SHELL" != "zshrc"]
+if [ $SHELL != "/usr/bin/zsh" ]
 then
 	sudo apt-get install zsh
 	echo "Shell installed"
+	echo "Copying template zshrc from repo..."
+	wget https://raw.githubusercontent.com/jctaillandier/ubuntu_setup/master/.zshrc?token=AHT7UAVFH4BSZIYPOV2FUMC6H36EW
 fi
-
-echo "Copying template zshrc from repo..."
-wget 

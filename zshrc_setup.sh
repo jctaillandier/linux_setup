c2dd60407a3961077a0f1c939ca128ad24f2d74f
J-C <31980034+jctaillandier@users.noreply.github.com>
2020-02-08 14:49:41 -0500

    Update zshrc_setup.sh

--- zshrc_setup.sh
+++ zshrc_setup.sh
@@ -10,7 +10,10 @@ then
 	echo "Shell installed"
 	echo "Copying template zshrc from repo..."
 	wget https://raw.githubusercontent.com/jctaillandier/ubuntu_setup/master/.zshrc
-	bash /bin/zsh
+	/bin/./zsh
 	# usermod --shell /bin/zsh $USER # ubuntu mate worked with this
 	echo "Installation completed. Log out and back in for changes to take effect"
+	if [ ! -d /var/.purpose]; then
+		echo "Think of creating a file in /var/.purpose to have machine purpose at the beginning of signature"
+	fi
 fi

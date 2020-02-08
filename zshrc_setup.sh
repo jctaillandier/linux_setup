8daf792bcf193fcc47f11d0a40b80f4c7cfad5c8
J-C <31980034+jctaillandier@users.noreply.github.com>
2020-02-08 14:45:17 -0500

    Update zshrc_setup.sh

--- zshrc_setup.sh
+++ zshrc_setup.sh
@@ -10,7 +10,7 @@ then
 	echo "Shell installed"
 	echo "Copying template zshrc from repo..."
 	wget https://raw.githubusercontent.com/jctaillandier/ubuntu_setup/master/.zshrc
-	chsh /bin/zsh $USER
+	bash /bin/zsh
 	# usermod --shell /bin/zsh $USER # ubuntu mate worked with this
 	echo "Installation completed. Log out and back in for changes to take effect"
 fi

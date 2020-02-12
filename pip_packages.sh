8f44542682450549ac26aa1375721a5dbaaedf96
J-C <31980034+jctaillandier@users.noreply.github.com>
2020-02-12 11:47:58 -0500

    Update and rename pip_packages to pip_packages.sh

---
+++ pip_packages.sh
@@ -0,0 +1,7 @@
+pip3 install sklearn --user
+pip3 install pandas --user
+pip3 install numpy  --user
+pip3 install torch --user
+pip3 install umap --user
+pip3 install tqdm --user
+pip3 install --user
